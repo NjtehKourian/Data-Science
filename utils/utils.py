f540a5e9c5a07a37c70088a011031fb62b328309
Njteh <njtehkk96@gmail.com>
2021-08-31 19:13:27 +0400

    statistics

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 #
 import math
 from validations import *
+from collections import Counter
 
 
 def vector_addition(v, w):
@@ -66,7 +67,7 @@ def scalar_product(v, w):
     :return: returns the dot product of the two vectors
     """
     are_vec_eq_len(v, w)
-    return sum([v_i*w_i for v_i, w_i in zip(v, w)])
+    return element_sum([v_i*w_i for v_i, w_i in zip(v, w)])
 
 
 def sum_of_comp_sq(v):
@@ -142,3 +143,117 @@ def square_sum_of_indices(i, j):
 
 def identity_matrix(i, j):
     return int(i == j)
+
+
+def element_sum(data):
+    """
+    basically the sum() function
+    :param data: dataset to be analyzed
+    :return: the sum of all the elements
+    """
+    is_vector(data)
+    total = data[0]
+    for el in data[1:]:
+        total += el
+    return total
+
+
+def mean(data):
+    """
+
+    :param data: dataset to be analyzed
+    :return: the mean of the dataset
+    """
+    is_vector(data)
+    return element_sum(data)/len(data)
+
+
+def median(data):
+    """
+
+    :param data: dataset to be analyzed
+    :return: the median of the dataset
+    """
+    is_vector(data)
+    sorted_data = sorted(data)
+    mid = len(data)//2
+    if len(data) % 2 == 1:
+        return sorted_data[mid]
+    return (sorted_data[mid+1] + sorted_data[mid])/2
+
+
+def quantile(data, percentage):
+    """
+
+    :param data: dataset to be analyzed
+    :param percentage: the percentile
+    :return: the quantile of the dataset at the percentile
+    """
+    is_vector(data)
+    is_number(percentage)
+    assert percentage > 0 and percentage < 1, "the percentage must be in range (0, 1)"
+    index = int(percentage*len(data))
+    return sorted(data)[index]
+
+
+def mode(data):
+    """
+
+    :param data: dataset to be analyzed
+    :return: the most common value in the dataset
+    """
+    is_vector(data)
+    counted = Counter(data)
+    max_occ = max(counted.values())
+    return [key for key in counted if counted[key] == max_occ]
+
+
+def mean_dif(data):
+    """
+
+    :param data: dataset to be analyzed
+    :return: a list containing the difference of each element of the data with the mean of the set
+    """
+    means = mean(data)
+    return [x - means for x in data]
+
+
+def variance(data):
+    """
+
+    :param data: dataset to be analyzed
+    :return: the variance of the dataset
+    """
+    is_vector(data)
+    variances = mean_dif(data)
+    return sum_of_comp_sq(variances)/(len(data)-1)
+
+
+def std(data):
+    """
+
+    :param data: dataset to be analyzed
+    :return: the standard deviation of the sample
+    """
+    return math.sqrt(variance(data))
+
+
+def covariance(data1, data2):
+    """
+
+    :param data1: first sample
+    :param data2: second sample
+    :return: the covariance of the two samples
+    """
+    eq_len(data1, data2)
+    x, y = mean_dif(data1), mean_dif(data2)
+    numerator = scalar_product(x, y)
+    return numerator/(len(x)-1)
+
+
+def correlation(data1, data2):
+    eq_len(data1, data2)
+    std1, std2 = std(data1), std(data2)
+    if std1 != 0 and std2 != 0:
+        return covariance(data1, data2) / (std1 * std2)
+    return 0
